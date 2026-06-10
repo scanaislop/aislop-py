711debedb197dfbd0d7a6a1eab80470291bc93db
Kehinde Olawuwo <olawuwokenny@gmail.com>
2026-06-10 23:21:47 +0100

    Fix PyPI launcher version drift

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -5,6 +5,5 @@ version="${1:?Usage: scripts/update-version.sh <version>}"
 
 perl -0pi -e "s#version = \"[^\"]+\"#version = \"$version\"#" pyproject.toml
 perl -0pi -e "s#__version__ = \"[^\"]+\"#__version__ = \"$version\"#" src/aislop_py/__init__.py
-perl -0pi -e "s#aislop@[0-9]+\\.[0-9]+\\.[0-9]+#aislop@$version#g" src/aislop_py/cli.py tests/test_cli.py
 
 echo "Updated Python launcher to aislop $version"
--- src/aislop_py/__init__.py
+++ src/aislop_py/__init__.py
@@ -1,3 +1,3 @@
 """Python launcher package for the aislop CLI."""
 
-__version__ = "0.10.2"
+__version__ = "0.11.0"
